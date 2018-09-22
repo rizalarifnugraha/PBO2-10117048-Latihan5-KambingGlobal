453b3ce0a36e0ba4882f4b77478512231ced31e6
rizalarifnugraha <43487664+rizalarifnugraha@users.noreply.github.com>
2018-09-22 19:41:41 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan5/kambingglobal/PBO210117048Latihan5KambingGlobal.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan5.kambingglobal;
+
+/**
+ *
+ * @author 
+ * Nama  : Rizal Arif Nugraha
+ * Kelas : PBO2 
+ * NIM   : 10117048
+ * Deskripsi Program : Program ini berisi program untuk menampilkan data yang 
+ * sudah ditambah dari program sebelumnya.
+
+ */
+public class PBO210117048Latihan5KambingGlobal {
+   // Variabel jumlahkambing menjadi variabel instance
+    int jumlahKambing = 88;
+   // Method untuk menampilkan jumlah kambing 
+    public void getJumlahKambing() {
+        System.out.println("Jumlah kambing: " + jumlahKambing);
+    }
+    public void tambahKambing() {
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PBO210117048Latihan5KambingGlobal kambingSusu = 
+                new PBO210117048Latihan5KambingGlobal();
+       // Menampilkan jumlah kambing yang ada saat program pertama x berjalan
+       kambingSusu.getJumlahKambing();
+       
+       // Menambah satu kambing
+       kambingSusu.tambahKambing();
+       
+       // Menampilkan jumlah kambing yang ada
+       kambingSusu.getJumlahKambing();
+    }
+    
+}
